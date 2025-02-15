8a5b4f4ef2551e1318a79d2a65a3e28aedd42468
Iruppave <iruppave@asu.edu>
2025-02-14 23:42:59 -0700

    Create Answer.java

---
+++ Answer.java
@@ -0,0 +1,33 @@
+public class Answer {
+    private int id;
+    private int questionId;
+    private String content;
+
+    public Answer(int id, int questionId, String content) {
+        this.id = id;
+        this.questionId = questionId;
+        this.content = content;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer [id=" + id + ", questionId=" + questionId + ", content=" + content + "]";
+    }
+}
